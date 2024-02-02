b5e7c412e378b51349e21940fee7cee03536281a
unknown <sunnylee3771@gmail.com>
2024-02-02 19:35:29 +0900

    test

---
+++ gateway/src/main/java/com/encore/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.encore.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
