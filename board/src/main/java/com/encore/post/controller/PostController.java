44680af7f7252201feee13f31a3852ed4ba8e2f2
GreatJang <1wkdwnsgur@gmail.com>
2024-02-06 19:46:59 +0900

    postCreate_update

--- board/src/main/java/com/encore/post/controller/PostController.java
+++ board/src/main/java/com/encore/post/controller/PostController.java
@@ -1,16 +1,22 @@
 package com.encore.post.controller;
 
+import com.encore.common.CommonResponse;
+import com.encore.post.domain.Post;
 import com.encore.post.dto.PostSaveReqDto;
 import com.encore.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
+@RequestMapping("/board/post")
 public class PostController {
     private final PostService postService;
 
@@ -19,10 +25,14 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/post/create")
-    public String postSave(PostSaveReqDto postSaveReqDto, HttpSession httpSession){
-            postService.save(postSaveReqDto);
-            return "OK";
+    @PostMapping("/create")
+    public ResponseEntity<CommonResponse> postCreate(PostSaveReqDto postSaveReqDto){
+        Post post = postService.create(postSaveReqDto);
+        System.out.println(postSaveReqDto.getTitle());
+        System.out.println(postSaveReqDto.getContents());
+        return new ResponseEntity<>(
+                new CommonResponse(HttpStatus.CREATED, "post succesfully create", post.getEmail())
+                , HttpStatus.CREATED);
     }
 
 }
--- board/src/main/java/com/encore/post/service/PostService.java
+++ board/src/main/java/com/encore/post/service/PostService.java
@@ -1,26 +1,46 @@
+import com.encore.ad
+
 package com.encore.post.service;
 
 import com.encore.post.domain.Post;
 import com.encore.post.dto.PostSaveReqDto;
 import com.encore.post.repository.PostRepository;
+import org.jboss.jandex.Main;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Service
-public class PostService {
+@Transactional
+public class PostService{
     private final PostRepository postRepository;
+    private final
 
     @Autowired
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
-//    public void save(PostSaveReqDto postSaveReqDto){
-//        Post post = new Post(postSaveReqDto.getTitle(),
-//                postSaveReqDto.getContents());
-//        postRepository.save(post);
-//    }
 
+    public Post create(PostSaveReqDto postSaveReqDto) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        Member post = postRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("not found email"));
+
+
+
+
+        Post new_post = Post.builder()
+                .title(postSaveReqDto.getTitle())
+                .contents(postSaveReqDto.getContents())
+                .build();
+        Post post = postRepository.save(new_post);
+        return post;
+    }
 }
