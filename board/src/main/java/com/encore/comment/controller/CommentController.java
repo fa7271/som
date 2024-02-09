7d6dffbf75d23babdc0434fcba074f3ffcfdc68e
GreatJang <1wkdwnsgur@gmail.com>
2024-02-09 14:17:52 +0900

    SOM-22_Comment_basic_update

--- board/src/main/java/com/encore/comment/controller/CommentController.java
+++ board/src/main/java/com/encore/comment/controller/CommentController.java
@@ -1,11 +1,17 @@
 package com.encore.comment.controller;
 
+import com.encore.comment.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/board/comment")
 public class CommentController {
+    private final CommentService commentService;
 
-
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
 }
--- board/src/main/java/com/encore/comment/repository/CommentRepository.java
+++ board/src/main/java/com/encore/comment/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.encore.comment.repository;
 
-public interface CommentRepository {
+import com.encore.comment.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
