489cbea9a606ef2573155675cb793b42217526f2
boseok <fa7271@naver.com>
2024-02-09 14:03:41 +0900

    SOM-23 하루 포스팅 갯수 제한

--- board/src/main/java/com/encore/post/repository/PostRepository.java
+++ board/src/main/java/com/encore/post/repository/PostRepository.java
@@ -4,6 +4,11 @@ import com.encore.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByEmailAndCreatedAtBetween(String email, LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
