3569b2e7e4468b685fb562a40c208b899a0d6677
boseok <fa7271@naver.com>
2024-02-19 15:33:42 +0900

    feature/refactory post_refactory

--- board/src/main/java/com/encore/post/dto/PostDetailResDto.java
+++ board/src/main/java/com/encore/post/dto/PostDetailResDto.java
@@ -1,10 +1,13 @@
 package com.encore.post.dto;
 
+import com.encore.post.domain.Post;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class PostDetailResDto {
     private Long id;
     private String email;
@@ -13,4 +16,19 @@ public class PostDetailResDto {
     private LocalDateTime createdAt;
 
     private int views;
+
+
+    public static PostDetailResDto ToPostDto(Post post) {
+        PostDetailResDtoBuilder builder = PostDetailResDto.builder();
+        builder
+                .id(post.getId())
+                .title(post.getTitle())
+                .views(post.getViews().size())
+                .contents(post.getContents())
+                .createdAt(post.getCreatedAt())
+                .email(post.getEmail())
+                .build();
+
+        return builder.build();
+    }
 }
