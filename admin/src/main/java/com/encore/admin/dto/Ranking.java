18ddc586f6be099772a6dfd4066c3c8e72203941
boseok <fa7271@naver.com>
2024-02-19 15:03:50 +0900

    SOM-23 내부통신 테스트

--- admin/src/main/java/com/encore/admin/dto/Ranking.java
+++ admin/src/main/java/com/encore/admin/dto/Ranking.java
@@ -9,11 +9,13 @@ import lombok.Data;
 public class Ranking {
     private String nickname;
     private Long ranking;
+    private String email;
 
     public static Ranking of(Member member) {
         return Ranking.builder()
                 .nickname(member.getNickname())
                 .ranking(member.getRanking())
+                .email(member.getEmail())
                 .build();
     }
 }
