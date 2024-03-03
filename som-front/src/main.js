53c3d725db899f2c0f8df24e276a8efc9e3fe506
GreatJang <1wkdwnsgur@gmail.com>
2024-03-03 10:58:25 +0900

    vue_postCreate_update

--- som-front/src/main.js
+++ som-front/src/main.js
@@ -5,21 +5,16 @@ import router from '@/router/index.js'
 import '@/assets/css/bootstrap.min.css' // bootstrap import
 import axios from 'axios';
 import '@fortawesome/fontawesome-free/css/all.css';
-import CKEditor from '@ckeditor/ckeditor5-vue3';
-
-Vue.use( CKEditor );
-
 
 const app = createApp(App);
-
 app.use(router);
-
 app.mount('#app');
 
 // 401응답의 경우 interceptor를 통해 공통적으로 토큰 제거 후 로그 관리
 axios.interceptors.response.use(response => response, error =>{
     if(error.response && error.response.status === 401){
         localStorage.clear();
+        alert("로그인 시간이 만료되었습니다. 다시 로그인 해주세요")
         window.location.href = "/login";
     }
     return Promise.reject(error)
--- som-front/src/views/PostCreate.vue
+++ som-front/src/views/PostCreate.vue
@@ -6,14 +6,16 @@
                 <label>게시글 제목:</label>
                 <input type="text" v-model="title" class="form-control" placeholder="제목을 입력하세요" style="height: 60px;">
             </div>
-            <!-- <div id="app" class="form-group">
-                <label for="editor">게시글 내용:</label>
-                <ckeditor :editor="editor" v-model="contents" :config="editorConfig"></ckeditor>
-            </div> -->
-            <div>
-              <textarea id="editor"></textarea>
+
+            <div class="form-group">
+              <label>게시글 내용:</label>
+              <textarea v-model="contents" class="form-control" placeholder="내용을 입력하세요" rows="10"></textarea>
             </div>
-          
+
+            <!-- <div>
+              <ckeditor :editor="editor" v-model="contents" :config="editorConfig"></ckeditor>
+            </div> -->
+        
             <div class="form-group">
                 <button type="submit" class="btn btn-primary mt-3">작성완료</button>
             </div>
@@ -21,111 +23,47 @@
     </div>
 </template>
 
-
 <script>
 import axios from 'axios';
-// import CKEditor from '@ckeditor/ckeditor5-vue';
-// import Editor from 'ckeditor5-custom-build/build/ckeditor'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
   export default {
-    name: 'editorComponent',
-    // components: {
-        // Use the <ckeditor> component in this view.
-          // ckeditor: CKEditor.component
-    // },
     props: ['isAdmin', 'pageTitle', 'id'],
     data() {
       return {
-        postList: [],
-        commentList: [],
         title: '',
         contents: '',
-        comment: '',
         isLoading: false,
         isEditing: false,
-        editedTitle: '',
-        // editedContent: '',
-        editor:null,
-        editorConfig: {
-          placeholder: "내용을 작성해 주세요",
         }
+      },
 
-
-        // editor: Editor,
-        // editorData: '<p>Content of the editor.</p>',
-        // editorConfig: {
-        //     placeholder: "내용을 작성해 주세요!",
-        //     ckfinder: {
-        //             uploadUrl: "http://localhost:8080/post/image/upload",
-        //             withCredentials: true,
-        //     }
-        // }
-      };
-    },
-    mounted(){
-      ClassicEditor.create(document.querySelector("#editor"), this.editorConfig).then(
-        editor => {
-          this.editor = editor;
-          this.editor.model.document.on("change", () => {
-            this.sendText();
-          });
-        }
-      )
-    },
     created() {
-      this.loadPosts(this.id);
-      this.loadComments();
       if (localStorage.getItem("token")) {
         this.isLogin = true;
         this.userRole = localStorage.getItem("role");
       }
     },
     methods: {
-      async loadPosts(id) {
-        this.isLoading = true;
-        try {
-          const token = localStorage.getItem('token');
-          const headers = token ? { Authorization: `Bearer ${token}` } : {};
-          const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/board/post/${id}/detail`, { headers });
-          this.postList = response.data.data;
-          this.title = this.postList.title;
-          this.contents = this.postList.contents;
-        } catch (error) {
-          console.error("데이터 불러오기 오류:", error);
-        }
-      },
-      async commentCreate() {
+      async postCreate() {
         this.isLoading = true;
         try {
           const formData = new FormData();
-          formData.append('comment', this.comment);
+          formData.append('title', this.title);
+          formData.append('contents', this.contents);
+
           const token = localStorage.getItem('token');
           const headers = token ? { Authorization: `Bearer ${token}` } : {};
-          await axios.post(`${process.env.VUE_APP_API_BASE_URL}/board/${this.id}/comment`, formData, { headers });
-          // 댓글이 등록된 후 댓글 목록을 다시 불러옴
-          await this.loadComments();
-          // 최신 댓글이 가장 위로 오도록 댓글 목록을 역순으로 정렬
-          this.commentList = this.commentList.reverse();
-          alert("댓글이 등록되었습니다.");
+          await axios.post(`${process.env.VUE_APP_API_BASE_URL}/board/post/create`, formData, { headers });
+          console.log(formData);
+
+          alert("게시글이 등록되었습니다.");
+          this.$router.push({ name: 'PostList' });
+
         } catch (error) {
-          console.error(error);
-          alert("입력값 확인 필요");
+
+          alert(error.response.data.error.label);
         }
         this.isLoading = false;
       },
-      async loadComments() {
-        this.isLoading = true;
-        try {
-          const token = localStorage.getItem('token');
-          const headers = token ? { Authorization: `Bearer ${token}` } : {};
-          const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/board/${this.id}/comment/list`, { headers });
-          this.commentList = response.data.data;
-        } catch (error) {
-          console.error("데이터 불러오기 오류:", error);
-        }
-      },
-
     }
   }
 </script>
-
