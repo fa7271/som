f589fc4c7dadd5eec47a72e708c92c4060cdecd3
boseok <fa7271@naver.com>
2024-03-05 17:48:04 +0900

    BadWordfiltering no editor

--- board/src/main/java/com/encore/common/filter/BadWordFiltering.java
+++ board/src/main/java/com/encore/common/filter/BadWordFiltering.java
@@ -21,7 +21,7 @@ public class BadWordFiltering extends HashSet<String> implements BadWords {
     }
 
     public String pre_change(String text) {
-        Pattern pattern = Pattern.compile("[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z\\s]");
+        Pattern pattern = Pattern.compile("[0-9ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z\\s]");
         Matcher matcher = pattern.matcher(text);
 
         StringBuilder filteredTextBuilder = new StringBuilder();
@@ -30,43 +30,6 @@ public class BadWordFiltering extends HashSet<String> implements BadWords {
         }
         return filteredTextBuilder.toString();
     }
-    public String export_html(String html_text) {
-        Document doc = Jsoup.parse(html_text);
-        StringBuilder filteredHtml = new StringBuilder();
-
-        // HTML 시작 부분 추가
-        filteredHtml.append("<html>\n <head></head>\n <body>\n");
-
-        for (Element element : doc.getAllElements()) {
-            String text = element.ownText();
-            String[] words = text.split("\\s+");
-            StringBuilder filteredLine = new StringBuilder();
-
-            for (String word : words) {
-                // 숫자로만 이루어진 경우는 그대로 유지
-                if (word.matches("[0-9]+")) {
-                    filteredLine.append(word).append(" ");
-                    continue;
-                }
-
-                // 욕설 필터링 적용
-                String filteredWord = change(word);
-                filteredLine.append(filteredWord).append(" ");
-            }
-
-            // 한 줄의 필터링된 텍스트를 해당 요소의 위치에 직접 대체
-            element.text(filteredLine.toString().trim());
-
-            // 필터링된 HTML 추가
-            filteredHtml.append(element.outerHtml());
-        }
-
-        // HTML 종료 부분 추가
-        filteredHtml.append("</body>\n</html>");
-
-        return filteredHtml.toString();
-    }
-
     public String change(String text) {
         for (String badWord : this) {
 //            대소문자를 무시하고 정규 표현식을 만듭니다.
