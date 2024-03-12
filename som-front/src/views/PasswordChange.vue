9dd02d5db6d159065b65b4261359f2131e632b0d
GreatJang <1wkdwnsgur@gmail.com>
2024-03-13 03:02:07 +0900

    DevOps_fix47

--- som-front/src/views/PasswordChange.vue
+++ som-front/src/views/PasswordChange.vue
@@ -51,7 +51,7 @@ export default {
         const token = localStorage.getItem('token');
         const headers = token ? { Authorization: `Bearer ${token}` } : {};
 
-        await axios.post(`${process.env.VUE_APP_API_BASE_URL}/admin/account/vertifycode`, requestData, { headers });
+        await axios.post(`${process.env.VUE_APP_API_BASE_ADMIN_URL}/admin/account/vertifycode`, requestData, { headers });
 
         // 성공적으로 작성된 경우 리다이렉트 또는 다른 처리 수행
         // this.$router.push({ name: 'LoginComponent' });
@@ -77,7 +77,7 @@ export default {
         const token = localStorage.getItem('token');
         const headers = token ? { Authorization: `Bearer ${token}` } : {};
 
-        await axios.get(`${process.env.VUE_APP_API_BASE_URL}/admin/account/findPassword/${this.email}`, {headers});
+        await axios.get(`${process.env.VUE_APP_API_BASE_ADMIN_URL}/admin/account/findPassword/${this.email}`, {headers});
 
         // 성공적으로 작성된 경우 리다이렉트 또는 다른 처리 수행
         // this.$router.push({ name: 'LoginComponent' });
