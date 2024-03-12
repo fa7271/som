decfd81e8f5c3d70b71223075799a0642d155469
GreatJang <1wkdwnsgur@gmail.com>
2024-03-12 15:21:06 +0900

    DevOps_fix34

--- admin/src/main/java/com/encore/admin/controller/HealthCheckController.java
+++ admin/src/main/java/com/encore/admin/controller/HealthCheckController.java
@@ -3,11 +3,14 @@ package com.encore.admin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpRequest;
+
 @RestController
 public class HealthCheckController {
 
     @GetMapping("/admin")
     public String healthcheck() {
+        System.out.println("여기까지 들어옴 ");
         return "OK";
     }
 }
