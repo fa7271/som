af48ccc8265e9082db6cad581db1825e960ccf47
GreatJang <1wkdwnsgur@gmail.com>
2024-03-13 11:20:31 +0900

    fix

--- board/src/main/java/com/encore/common/config/BadWordIntercepter.java
+++ board/src/main/java/com/encore/common/config/BadWordIntercepter.java
@@ -1,7 +1,7 @@
 package com.encore.common.config;
 
 
-import com.encore.common.filter.BadWordFiltering;
+//import com.encore.common.filter.BadWordFiltering;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
