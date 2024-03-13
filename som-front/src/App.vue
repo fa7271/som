2dd2ce48055bd091c4d39da045d83a2762feab75
GreatJang <1wkdwnsgur@gmail.com>
2024-03-13 13:24:53 +0900

    DevOps_fix 73

--- som-front/src/App.vue
+++ som-front/src/App.vue
@@ -11,12 +11,12 @@
 // import HomeComponent from "./components/HomeComponent.vue";
 // 절대경로 : @는 jsconfig의 경로 키워드로 선언
 import HeaderComponent from "@/components/HeaderComponent.vue";
-import FooterComponent from "@/components/FooterComponent.vue";
+// import FooterComponent from "@/components/FooterComponent.vue";
 export default {
     // 사용할 하위 컴포넌트 목록 나열
     components:{
       HeaderComponent,
-      FooterComponent,
+      // FooterComponent,
     }
 }
 </script>
