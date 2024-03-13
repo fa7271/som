6f6093adb5dcbdaac502db4973997d8b9873100e
GreatJang <1wkdwnsgur@gmail.com>
2024-03-13 10:03:37 +0900

    DevOps_fix55

--- board/src/main/java/com/encore/common/config/WebConfig.java
+++ board/src/main/java/com/encore/common/config/WebConfig.java
@@ -12,25 +12,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final BadWordFiltering badWordFiltering;
-
-    public WebConfig(BadWordFiltering badWordFiltering) {
-        this.badWordFiltering = badWordFiltering;
-    }
-
-    @Bean
-    public PasswordEncoder makePassword() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new BadWordIntercepter(badWordFiltering))
-                .addPathPatterns("/board/board/post/create")  // 인터셉터를 적용할 경로 패턴
-                .addPathPatterns("/board/board/post/*/update")
-                .addPathPatterns("/board/board/*/comment")
-                .excludePathPatterns("/board/board/post/list/**, /css/**", "/fonts/**");
-    }
+//    private final BadWordFiltering badWordFiltering;
+//
+//    public WebConfig(BadWordFiltering badWordFiltering) {
+//        this.badWordFiltering = badWordFiltering;
+//    }
+//
+//    @Bean
+//    public PasswordEncoder makePassword() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new BadWordIntercepter(badWordFiltering))
+//                .addPathPatterns("/board/board/post/create")  // 인터셉터를 적용할 경로 패턴
+//                .addPathPatterns("/board/board/post/*/update")
+//                .addPathPatterns("/board/board/*/comment")
+//                .excludePathPatterns("/board/board/post/list/**, /css/**", "/fonts/**");
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
